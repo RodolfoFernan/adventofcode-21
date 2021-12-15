29cf40ba550841fce4364f514832df6ca7402b8f
RodolfoFernan <Rodolfoengineering@gmail.com>
2021-12-15 10:53:58 -0300

    Update part1.java
    modificado

--- Day-01/java/B33fb0n3/part1.java
+++ Day-01/java/B33fb0n3/part1.java
@@ -1,12 +1,12 @@
 public class MyClass {
     public static void main(String args[]) {
-        int increases = 0;
+        int increases = 2;
 
-        int lastone = 0;
+        int lastone = 1;
 
         for(int i = 0; i < args.length - 2; i++) {
 
-            if(Integer.parseInt(args[i]) > lastone)
+            if(Integer.parseInt(args[i] ) > lastone)
                 increases++;
 
             lastone = Integer.parseInt(args[i]);
